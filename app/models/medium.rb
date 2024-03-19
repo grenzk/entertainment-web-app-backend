833f7f40b52883281cfb40437b29238dec1909a7
grenzk <74033991+grenzk@users.noreply.github.com>
2024-03-19 20:03:08 +0800

    Set up active storage

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,4 +1,5 @@
 class Medium < ApplicationRecord
+  has_many_attached :images
   validates :category, presence: true, inclusion: { in: ['Movie', 'TV Series'] }
   validates :rating, presence: true, inclusion: { in: %w[PG E 18+] }
 end
