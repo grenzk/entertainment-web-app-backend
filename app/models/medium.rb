d16380f38c92fbe3bd7ab17a24ac2c8a2dffd67a
grenzk <74033991+grenzk@users.noreply.github.com>
2024-03-19 16:15:37 +0800

    Modify medium model validation

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,4 +1,4 @@
 class Medium < ApplicationRecord
-  validates :category, inclusion: { in: ['Movie', 'TV Series'] }
-  validates :rating, inclusion: { in: %w[PG E 18+] }
+  validates :category, presence: true, inclusion: { in: ['Movie', 'TV Series'] }
+  validates :rating, presence: true, inclusion: { in: %w[PG E 18+] }
 end
