465b896ce6ea36e51d188630cd2258c5f9bb4cea
grenzk <74033991+grenzk@users.noreply.github.com>
2024-03-21 19:59:47 +0800

    Change attribute name

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -5,7 +5,7 @@ class Medium < ApplicationRecord
   validates :year, presence: true
   validates :category, presence: true, inclusion: { in: ['Movie', 'TV Series'] }
   validates :rating, presence: true, inclusion: { in: %w[PG E 18+] }
-  validates :is_trending, presence: true
+  validates :trending, presence: true
   validates :images, attached: true, content_type: %w[image/png image/jpeg],
                      size: { less_than: 5.megabytes }
 end
