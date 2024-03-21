fb958cdf9282d8f8c633ca5a0f7b643606558255
grenzk <74033991+grenzk@users.noreply.github.com>
2024-03-21 13:14:30 +0800

    Add required validations

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,5 +1,9 @@
 class Medium < ApplicationRecord
   has_many_attached :images
+
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :year, presence: true
   validates :category, presence: true, inclusion: { in: ['Movie', 'TV Series'] }
   validates :rating, presence: true, inclusion: { in: %w[PG E 18+] }
+  validates :is_trending, presence: true
 end
