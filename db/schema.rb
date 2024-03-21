7cb4dac4aa494e6a17fa164b1c0f434a99cac304
grenzk <74033991+grenzk@users.noreply.github.com>
2024-03-21 17:47:04 +0800

    Rename column is_trending to trending

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_19_115626) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_21_094343) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_19_115626) do
     t.integer "year"
     t.string "category"
     t.string "rating"
-    t.boolean "is_trending"
+    t.boolean "trending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
