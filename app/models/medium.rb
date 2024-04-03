705cea2845afe558f3ead5c79b786a26a2708dbc
grenzk <74033991+grenzk@users.noreply.github.com>
2024-04-03 20:13:40 +0800

    Remove active storage validations

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,11 +1,7 @@
 class Medium < ApplicationRecord
-  has_many_attached :images
-
   validates :title, presence: true, length: { maximum: 30 }
   validates :year, presence: true
   validates :category, presence: true, inclusion: { in: ['Movie', 'TV Series'] }
   validates :rating, presence: true, inclusion: { in: %w[PG E 18+] }
   validates :trending, presence: true
-  validates :images, attached: true, content_type: %w[image/png image/jpeg],
-                     size: { less_than: 5.megabytes }
 end
