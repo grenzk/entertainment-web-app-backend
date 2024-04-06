302da76ec5baf579eb0700c72ef195f7ddb6cd51
grenzk <74033991+grenzk@users.noreply.github.com>
2024-04-06 17:17:14 +0800

    Associate medium with bookmarks

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,4 +1,6 @@
 class Medium < ApplicationRecord
+  has_many :bookmarks
+
   validates :title, presence: true, length: { maximum: 30 }
   validates :year, presence: true
   validates :category, presence: true, inclusion: { in: ['Movie', 'TV Series'] }
