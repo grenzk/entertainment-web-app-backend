43fbdd0125e1db6e81ab654ca347506de43967e9
grenzk <74033991+grenzk@users.noreply.github.com>
2024-04-09 20:11:13 +0800

    Allow adding and removing bookmarks

--- app/controllers/api/v1/bookmarks_controller.rb
+++ app/controllers/api/v1/bookmarks_controller.rb
@@ -1,5 +1,38 @@
 class API::V1::BookmarksController < ApplicationController
+  before_action :set_medium, only: %i[show destroy]
+
   def index
     render json: Bookmark.includes(:medium).all, except: %i[id created_at updated_at]
+  end
+
+  def show
+    render json: @medium.bookmarks.find_by(medium_id: params[:id])
+  end
+
+  def create
+    bookmark = Bookmark.new(bookmark_params)
+
+    if bookmark.save
+      render json: bookmark, status: :created
+    else
+      render json: bookmark.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    bookmark = @medium.bookmarks.find_by(medium_id: params[:id])
+
+    bookmark.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_medium
+    @medium = Medium.find(params[:id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:medium_id)
   end
 end
