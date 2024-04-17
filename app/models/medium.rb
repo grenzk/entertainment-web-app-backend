02168450ea1df57f55fc5e063c0234bbb374cd95
grenzk <74033991+grenzk@users.noreply.github.com>
2024-04-17 15:13:12 +0800

    Order data by created_at timestamp

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -6,4 +6,6 @@ class Medium < ApplicationRecord
   validates :category, presence: true, inclusion: { in: ['Movie', 'TV Series'] }
   validates :rating, presence: true, inclusion: { in: %w[PG E 18+] }
   validates :trending, presence: true
+
+  scope :ordered, -> { order(created_at: :asc) }
 end
