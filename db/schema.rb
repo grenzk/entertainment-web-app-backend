30473c12e03a1b0460590e72434dd8ca54eec685
grenzk <74033991+grenzk@users.noreply.github.com>
2024-04-25 18:04:12 +0800

    Update database schema with users table and denylist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_06_091100) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_25_094132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_06_091100) do
     t.index ["medium_id"], name: "index_bookmarks_on_medium_id"
   end
 
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
+
   create_table "media", force: :cascade do |t|
     t.string "title"
     t.integer "year"
@@ -29,6 +37,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_06_091100) do
     t.boolean "trending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "bookmarks", "media"
