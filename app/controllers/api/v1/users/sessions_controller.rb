502339eb9ff2f979e852cb15eebd92330f1d4e43
grenzk <74033991+grenzk@users.noreply.github.com>
2024-05-02 14:51:20 +0800

    Add minor fix

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -8,7 +8,7 @@ class API::V1::Users::SessionsController < Devise::SessionsController
   end
 
   def respond_to_on_destroy
-    log_out_success && return unless current_user
+    log_out_success && return if current_user
 
     log_out_failure
   end
