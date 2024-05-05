19d1dd3501368ce30dd7d9c18fb0244444d070ad
grenzk <74033991+grenzk@users.noreply.github.com>
2024-05-05 21:25:28 +0800

    Update auth controllers

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -4,7 +4,11 @@ class API::V1::MembersController < ApplicationController
   def show
     user = user_from_token
 
-    render json: { message: "If you see this, you're in!", user: }
+    if user
+      render json: { user: user.as_json(except: :updated_at) }, status: :ok
+    else
+      render json: { message: 'User not found.' }, status: :not_found
+    end
   end
 
   private
@@ -12,8 +16,8 @@ class API::V1::MembersController < ApplicationController
   def user_from_token
     jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
                              Rails.application.credentials.devise[:jwt_secret_key]).first
-
     user_id = jwt_payload['sub']
+
     User.find(user_id.to_s)
   end
 end
--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -4,7 +4,7 @@ class API::V1::Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(_resource, _opts = {})
-    render json: { message: 'You are logged in.', user: current_user }, status: :ok
+    render json: { message: 'Logged in successfully.', user: current_user.as_json(except: :updated_at) }, status: :ok
   end
 
   def respond_to_on_destroy
@@ -14,10 +14,10 @@ class API::V1::Users::SessionsController < Devise::SessionsController
   end
 
   def log_out_success
-    render json: { message: 'You are logged out.' }, status: :ok
+    render json: { message: 'Logged out successfully.' }, status: :ok
   end
 
   def log_out_failure
-    render json: { message: 'Hmm nothing happened.' }, status: :unauthorized
+    render json: { message: "Couldn't find an active session." }, status: :unauthorized
   end
 end
