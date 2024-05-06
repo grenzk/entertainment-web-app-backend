7bdc2b1b9494dc76441d11c9b7bae51bb2c33adc
grenzk <74033991+grenzk@users.noreply.github.com>
2024-05-06 21:03:52 +0800

    Associate user with bookmarks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :bookmarks
+
   devise :database_authenticatable, :registerable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
